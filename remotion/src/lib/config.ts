1ec6bd2c47d4e5e11b7db2dbbdf24382a6f411cf
tlk0s <tolko.krustev@gmail.com>
2026-06-27 07:38:16 +0300

    chore: remove remotion video project and superpowers docs

--- remotion/src/lib/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-  phone: process.env.REMOTION_PHONE ?? '+359 88 888 8888',
-  service: process.env.REMOTION_SERVICE ?? 'Електротехник в София',
-}
--- remotion/src/lib/fonts.ts
+++
@@ -1,6 +0,0 @@
-import { loadFont } from '@remotion/google-fonts/Inter'
-
-export const { fontFamily } = loadFont('normal', {
-  weights: ['400', '600', '700'],
-  subsets: ['latin', 'cyrillic'],
-})
